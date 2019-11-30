8c6dd97dbd00fc3e5ec51b338a0e6611de49ed4a
dataning <bnnl@leeds.ac.uk>
2019-11-30 10:42:03 +0000

    1

--- my_app/app.R
+++ my_app/app.R
@@ -1,37 +1,29 @@
 library(shiny)
 
-# Define UI for app that draws a histogram ----
+# Second part, define UI for the Shiny app
 ui <- fluidPage(
-  
   # App title ----
-  titlePanel("Hello Shiny!"),
-  
+  titlePanel("Hello Shiny Learner!"),
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
-    
     # Sidebar panel for inputs ----
     sidebarPanel(
-      
       # Input: Slider for the number of bins ----
       sliderInput(inputId = "bins",
                   label = "Number of bins:",
-                  min = 1,
+                  min = 5,
                   max = 50,
                   value = 30)
-      
     ),
-    
     # Main panel for displaying outputs ----
     mainPanel(
-      
       # Output: Histogram ----
       plotOutput(outputId = "distPlot")
-      
     )
   )
 )
 
-# Define server logic required to draw a histogram ----
+# Third part, define server logic required to draw a histogram
 server <- function(input, output) {
   
   # Histogram of the Old Faithful Geyser Data ----
@@ -43,21 +35,13 @@ server <- function(input, output) {
   #    re-executed when inputs (input$bins) change
   # 2. Its output type is a plot
   output$distPlot <- renderPlot({
-    
     x    <- faithful$waiting
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    hist(x, breaks = bins, col = "#75AADB", border = "white",
+    hist(x, breaks = bins, col = "#75AADB", border = "orange",
          xlab = "Waiting time to next eruption (in mins)",
          main = "Histogram of waiting times")
-    
   })
-  
 }
 
-
-
-# See above for the definitions of ui and server
-
-
+# Fourth part, tie up ui and server
 shinyApp(ui = ui, server = server)
